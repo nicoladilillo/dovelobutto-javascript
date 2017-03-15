d256b93c6728c28a4211cdd39a4e0cff6dc34389
Nicola Dilillo <nicoladilillo98@gmail.com>
2017-03-15 10:29:33 +0100

    Handle product name

--- web/assets/js/controller.js
+++ web/assets/js/controller.js
@@ -16,6 +16,14 @@ function handleProduct(product) {
 function init($page, data) {
   if(!data) data = {};
 
+  $page.find('.js--bin-name').text(data.binName);
+  $page.find('.js--product-name').text(data.productName);
+  $page.find('.js--email').text(data.email);
+  $page.find('.js--bin-image').attr('src', '/assets/images/bin-' + data.binId + '.png');
+  $page.find('.js--bin-container')
+    .attr('class', $page.find('.js--bin-container').data('class'))
+    .addClass('bin--' + data.binId);
+
   $page.find('.js--search-form').off('submit').on('submit', function(e) {
     e.preventDefault();
     var productName = this.name.value;
