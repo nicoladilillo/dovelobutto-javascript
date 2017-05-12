bb9550cf272ef4eeded09f650e15cc3ec98cc04b
Nicola Dilillo <nicoladilillo98@gmail.com>
2017-05-12 17:41:05 +0200

    Create index with Silex

--- web/index.php
+++ web/index.php
@@ -5,8 +5,12 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/assets/views',
+));
+
 $app->get('/', function () use ($app) {
-    return 'Hello';
+    return $app['twig']->render('index.html');
 });
 
 $app->run();
